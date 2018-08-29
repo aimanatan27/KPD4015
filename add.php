370dc7662ea915e87508992ffe49884220f07857
Noor Aiman <Noor Aiman@AtanTheProgrammer>
2018-08-29 23:06:26 +0800

    Commit Projek 1.0

---
+++ add.php
@@ -0,0 +1,67 @@
+<?php
+//PANGGIL FAIL SAMBUNG KE PANGKALAN DATA
+include_once("config.php");
+
+if(isset($_POST['Submit'])) {
+    $nama = $_POST['nama'];
+    $harga = $_POST['harga'];
+    $id = $_POST['id'];
+    
+    
+//TAMBAH REKOD KE PANGKALAN DATA
+    $result = mysqli_query($mysqli, "INSERT INTO produk(nama,harga,id) "
+            . "VALUES('$nama','$harga','$id')");
+    
+//PAPAR MESEJ IKA TAMBAH REKOD BERJAYA
+echo "<script>alert('Tambah maklumat berjaya'); "
+    . "window.location='index.php'</script>";
+
+}
+ else {
+    
+
+?>
+
+<html>
+<head>
+<title>TAMBAH REKOD</title>
+</head>
+<body>
+<center> <h2>TAMBAH REKOD BARANGAN DAN HARGA</h2>
+    <fieldset>
+        <form action="add.php" method="post" name="form1">
+            <table width="25%" border="0">
+                <tr>
+                    <td>Nama Produk</td>
+                    <td><input type="text" name="nama"></td>
+                </tr>
+                
+                
+                <tr>
+                    <td>Harga</td>
+                    <td><input type="text" name="harga"></td>
+                </tr>
+                <tr>
+                    <td>ID</td>
+                    <td><input type="text" name="id"></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="Submit" value="Add"></td>
+                </tr>
+            </table>
+        </form>
+       
+        <BR><a href="index.php">Kembali ke Laman Utama</a>
+    </fieldset>
+</center>
+<?php
+ }
+ ?>
+
+</body>
+
+</html>
+
+
+
---
+++ error.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>404 Not Found</title>
+</head>
+<body>
+	<h1>Server Error in '/ErrorSample' Application.</h1>
+<h2>The resource cannot be found.</h2>
+<p><b>Description:</b> HTTP 404. The resource for (or one of its dependencies) could have been removed,
+	had its name changed, or is temporarily unavailable. Please review the following URL and make sure that it is spelled correctly.
+<p><b>Requested URL:</b> /errorsample/Trial.aspx</p>
+
+<p><b>Version Information:</b> Runtime Build:1.0.2914.16; ASP.NET Build:1.4000.2914.16</p>
+</body>
+</html>
+
